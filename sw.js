5c6899508c927f455f7e59b51106564b5a8f2b1f
befflus <befflus@hotmail.no>
2018-08-25 13:02:13 +0200

    Set the ignoreSearch to a true value in the match() method. This renders the restaurant.html page instead of an error page when offline.

--- sw.js
+++ sw.js
@@ -46,7 +46,7 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-		caches.match(event.request).then((response) => {
+		caches.match(event.request, {ignoreSearch: true}).then((response) => {
 			if (response) {
 				return response;
 			}
